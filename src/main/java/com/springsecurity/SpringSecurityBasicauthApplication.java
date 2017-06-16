33988161855a5011afc533a412705e59d76d429b
sorabhm <sorabhm@gmail.com>
2017-06-16 08:35:56 +0530

    basic project setup

---
+++ src/main/java/com/springsecurity/SpringSecurityBasicauthApplication.java
@@ -0,0 +1,12 @@
+package com.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityBasicauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityBasicauthApplication.class, args);
+	}
+}
